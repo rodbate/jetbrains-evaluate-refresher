82b7798c4bb6629401bcab03394f5b5f39f7cc01
GitHub Action <action@github.com>
2021-03-03 07:58:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/rodbate/intellijevaluaterefresher/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rodbate.intellijevaluaterefresher.services
+
+import com.github.rodbate.intellijevaluaterefresher.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
